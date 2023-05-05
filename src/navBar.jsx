3756e0c8b62c21ff430adda7028db4c25fea5363
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2023-05-06 05:25:14 +0530

    first commit

---
+++ src/navBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+  <>
+    <nav className="top">
+        <li>Home</li>
+        <li>About</li>
+        <li>contact</li>
+    </nav>
+  </>
+  )
+}
+
+export default NavBar
