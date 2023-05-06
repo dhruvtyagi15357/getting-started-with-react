e4d9f06fac2632cf13aa8434ba5e90bed4a639a3
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2023-05-06 15:22:46 +0530

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 // import { useState } from 'react'
 import './App.css'
-import NavBar from './NavBar'
+import NavBar from './components/navBar'
 
 function App() {
   let name="Dhruv Tyagi";
-
+  let aboutText="This is the about section";
   return (
       <>
         <NavBar />
-        <h1>Hello there! I am {name} </h1>
 
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem sit, ex quibusdam eligendi natus aperiam exercitationem unde ratione tenetur iure dolore repellat excepturi voluptatem repudiandae ipsam similique itaque magni illum doloremque libero sint qui? Repellat voluptates ipsum ducimus iure maxime neque maiores fuga iste et ullam! Sint blanditiis corrupti excepturi sunt aliquam obcaecati quisquam, assumenda placeat ex aut provident delectus fugiat similique voluptatem natus culpa laboriosam quod nostrum quis minus aperiam labore ad! Sit quia vero quibusdam reprehenderit temporibus nobis, rerum inventore amet animi porro possimus. Veritatis ad laboriosam neque ut necessitatibus autem perspiciatis quia officia, laudantium cum voluptate, provident earum totam odit pariatur, eaque magnam illo repellendus nulla sint aliquid delectus reiciendis aliquam. Neque facere ea, exercitationem adipisci porro inventore eum similique quam pariatur modi veniam maiores necessitatibus quis, dolores placeat officiis possimus id saepe, dolor dolorem atque! Exercitationem laborum dolor assumenda ut quas, minus modi explicabo ab necessitatibus molestias voluptatibus nesciunt ipsum deleniti distinctio, corrupti repudiandae nisi culpa labore, aut nostrum molestiae magni eaque ratione? Sapiente odio id soluta eaque, eius enim aut ipsa inventore qui consectetur vel ut quam mollitia dolorum quasi ex expedita nesciunt autem sint. Repellat suscipit nulla dolor nam numquam aperiam doloremque quas facere!
-        </p>
       </>
   )
 }
